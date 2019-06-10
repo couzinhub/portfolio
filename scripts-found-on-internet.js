684f25826d40999b4d32a59be2be0dc7aa6091f8
Hubert <couzin.hub@gmail.com>
2019-06-09 21:26:31 -0700

    trials and errors

--- scripts-found-on-internet.js
+++ scripts-found-on-internet.js
@@ -1,14 +1,20 @@
+function copyText(){
+	var text = document.getElementById("userInput");
+	text.select();
+	document.execCommand("copy");
+}
+
 
 function myFunction() {
-  var copyText = document.getElementById("myInput");
-  copyText.select();
-  document.execCommand("copy");
-  
-  var tooltip = document.getElementById("myTooltip");
-  tooltip.innerHTML = "Copied: " + copyText.value;
+	var copyText = document.getElementById("myInput");
+	copyText.select();
+	document.execCommand("copy");
+	
+	var tooltip = document.getElementById("myTooltip");
+	tooltip.innerHTML = "Copied: " + copyText.value;
 }
 
 function outFunc() {
-  var tooltip = document.getElementById("myTooltip");
-  tooltip.innerHTML = "Copy to clipboard";
+	var tooltip = document.getElementById("myTooltip");
+	tooltip.innerHTML = "Copy to clipboard";
 }
